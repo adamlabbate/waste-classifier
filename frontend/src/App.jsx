3d8d192287e5853136e8986dce075b0484155b51
Adam Labbate <adamlabbate@gmail.com>
2026-05-21 13:34:56 -0700

    update API URL to point to huggingface space

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ function App() {
     formData.append('image', image)
 
     try {
-      const res = await fetch('http://127.0.0.1:5000/predict', {
+      const res = await fetch('https://adamlabbate-waste-classifier.hf.space/predict', {
         method: 'POST',
         body: formData,
       })
